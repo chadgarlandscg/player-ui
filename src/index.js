489ed3522ae6132e78a4f2b91037624b40cc2317
chadgarlandscg <chadgarlandscg@gmail.com>
2020-07-20 16:46:37 -0600

    Refresh players when a new one is added

--- src/index.js
+++ src/index.js
@@ -7,14 +7,21 @@ const playerListContainer = document.getElementById("player-list-container");
 
 button.addEventListener('click', (event) => {
     const nameInput = document.getElementById("my-name-input");
-    axios.post("http://localhost:9999/players", {name: nameInput.value});
-});
-
-axios.get("http://localhost:9999/players").then((response) => {
-    const players = response.data;
-    players.forEach(player => {
-        const paragraphElement = document.createElement('p');
-        paragraphElement.innerText = player.name;
-        playerListContainer.append(paragraphElement);
+    axios.post("http://localhost:9999/players", {name: nameInput.value}).then(() => {
+        loadPlayers();
     });
 });
+
+function loadPlayers() {
+    axios.get("http://localhost:9999/players").then((response) => {
+        const players = response.data;
+        playerListContainer.innerHTML = "";
+        players.forEach(player => {
+            const paragraphElement = document.createElement('p');
+            paragraphElement.innerText = player.name;
+            playerListContainer.append(paragraphElement);
+        });
+    });
+}
+
+loadPlayers();
