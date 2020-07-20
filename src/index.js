9e96ef6d3810b8b445a5f0537420d45a1cb32dd3
chadgarlandscg <chadgarlandscg@gmail.com>
2020-07-20 16:26:37 -0600

    Add basic UI

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const button = document.getElementById("my-button");// way to get the button
+console.log(button.innerText);
+
+const playerListContainer = document.getElementById("player-list-container");
+
+button.addEventListener('click', (event) => {
+    const nameInput = document.getElementById("my-name-input");
+    axios.post("http://localhost:9999/players", {name: nameInput.value});
+});
+
+axios.get("http://facebook-server.com/players").then((response) => {
+    const players = response.data;
+    players.forEach(player => {
+        const paragraphElement = document.createElement('p');
+        paragraphElement.innerText = player.name;
+        playerListContainer.append(paragraphElement);
+    });
+});
