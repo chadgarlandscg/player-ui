74382281bc5b48c7d1e280e481391d51c49db08d
chadgarlandscg <chadgarlandscg@gmail.com>
2020-07-20 16:57:03 -0600

    Add player on enter event

--- src/index.js
+++ src/index.js
@@ -6,11 +6,25 @@ console.log(button.innerText);
 const playerListContainer = document.getElementById("player-list-container");
 
 button.addEventListener('click', (event) => {
+    addPlayer();
+});
+
+const input = document.getElementById("my-name-input");
+input.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        addPlayer();
+      }
+});
+
+function addPlayer() {
     const nameInput = document.getElementById("my-name-input");
     axios.post("http://localhost:9999/players", {name: nameInput.value}).then(() => {
         loadPlayers();
     });
-});
+}
 
 function loadPlayers() {
     axios.get("http://localhost:9999/players").then((response) => {
