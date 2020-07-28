2c44a44bc827ec7a2e3471d13bbae07f5c94719e
chadgarlandscg <chadgarlandscg@gmail.com>
2020-07-28 16:27:17 -0600

    Install React

--- src/playerApi.js
+++ src/playerApi.js
@@ -2,20 +2,12 @@ const axios = require("axios");
 
 export function addPlayer() {
     const nameInput = document.getElementById("my-name-input");
-    axios.post("http://localhost:9999/players", {name: nameInput.value}).then(() => {
-        loadPlayers();
-    });
+    return axios.post("http://localhost:9999/players", {name: nameInput.value});
 }
 
 export function loadPlayers() {
     axios.get("http://localhost:9999/players").then((response) => {
         const players = response.data;
-        const playerListContainer = document.getElementById("player-list-container");
-        playerListContainer.innerHTML = "";
-        players.forEach(player => {
-            const paragraphElement = document.createElement('p');
-            paragraphElement.innerText = player.name;
-            playerListContainer.append(paragraphElement);
-        });
+        return players;
     });
 }
