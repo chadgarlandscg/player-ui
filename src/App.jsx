470303dffa147a60b03d5a509004d99089af4806
chadgarlandscg <chadgarlandscg@gmail.com>
2020-07-29 16:36:36 -0600

    Extract Form to new file & move event handlers to component class methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { addPlayer } from './playerApi';
+import { Form } from './Form';
+
 export class App extends React.Component {
     constructor() {
         super();
@@ -9,34 +11,10 @@ export class App extends React.Component {
         return (
             <div>
                 <h5>Hello, Sergio and Sebas</h5>
-                <Form buttonTitle="Click me pretty please!"/>
-                <Form/>
+                <Form buttonTitle="Click me pretty please!" onSubmit={(state) => console.log(state)}/>
+                <Form onSubmit={(state) => console.log(state)}/>
             </div>
         );
     }
 }
 
-class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputValue: ""
-        };
-    }
-    render() {
-        return (
-            <div>
-                <input value={this.state.inputValue} onChange={(event) => {
-                    const newState = {
-                        inputValue: event.target.value
-                    };
-                    this.setState(newState);
-                    console.log(event.target.value);
-                }}/>
-                <button onClick={() => {
-                    console.log(this.state)
-                }}>{this.props.buttonTitle || "Test!"}</button>
-            </div>
-        )
-    }
-}
---
+++ src/Form.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+export class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ""
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onButtonClick = this.onButtonClick.bind(this);
+    }
+
+    onInputChange(event) {
+        const newState = {
+            inputValue: event.target.value
+        };
+        this.setState(newState);
+        console.log(event.target.value);
+    }
+
+    onButtonClick() {
+        this.props.onSubmit(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <input value={this.state.inputValue} onChange={this.onInputChange}/>
+                <button onClick={this.onButtonClick}>{this.props.buttonTitle || "Test!"}</button>
+            </div>
+        );
+    }
+}
