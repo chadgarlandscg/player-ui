3f8daa0e15f5f3867d4ddeb67b3d184569ebf9dc
chadgarlandscg <chadgarlandscg@gmail.com>
2020-08-15 17:20:27 -0600

    Get attack and defend working

--- index.js
+++ index.js
@@ -1,76 +1,87 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var _this = this;
-console.log("Hello, world!");
-var x = "test";
-function addNumbers(x, y) {
-    return x + y;
+class Attack {
+    constructor(attacker, defender) {
+        this.attacker = attacker;
+        this.defender = defender;
+    }
+    get damage() {
+        return this.attacker.power - this.defender.resistance;
+    }
 }
-var y = addNumbers(2, 3);
-function addNames(n1, n2) {
-    console.log(n1 + n2);
-    return n1 + n2;
+class Fighter {
+    constructor(attacks) {
+        this._health = new FighterHealth(attacks);
+    }
+    get basePower() { return 2.0; }
+    ;
+    get baseResistance() { return 1.0; }
+    ;
+    attack(defender) {
+        return new Attack(this, defender);
+    }
+    get power() {
+        return this.basePower;
+    }
+    defend(attack) {
+        return this.health.register(attack);
+    }
+    get resistance() {
+        return this.baseResistance;
+    }
+    get health() {
+        return this._health;
+    }
 }
-var z = addNames("sergio", "chad");
-console.log(z);
-var p1 = {
-    name: "Chad",
-    health: 100,
-    power: 1,
-    attack: function (player) {
-        player.health = player.health - _this.power;
-    }
-};
-var p2 = {
-    name: "Chad",
-    health: 100,
-    power: 1,
-    attack: function (player) {
-        player.health = player.health - _this.power;
-    }
-};
-var Player = /** @class */ (function () {
-    function Player(initialName, initialPower) {
-        this.health = 100;
-        this.name = initialName;
-        this.power = initialPower;
-    }
-    Player.prototype.attack = function (player) {
-        player.health = player.health - this.power;
-    };
-    return Player;
-}());
-var Barbarian = /** @class */ (function (_super) {
-    __extends(Barbarian, _super);
-    function Barbarian() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Barbarian.prototype.attack = function (player) {
-        player.health = player.health - this.power * 2;
-    };
-    return Barbarian;
-}(Player));
-var fighters = [new Barbarian("Steve", 10), new Player("Test", 10)];
-handleWar(fighters);
-function handleWar(attackers) {
-    attackers.forEach(function (p) {
-        p.attack(playerOne);
-    });
+class Inventory {
 }
-var playerOne = new Player("Sergio", 100);
-var playerTwo = new Player("Chad", 10);
-playerOne.attack(playerTwo);
-console.log(playerTwo);
-// playerOne.attack(playerTwo);
-// console.log("Player two has " + playerTwo.health + " health remaining");
+class Charge {
+    constructor(price) {
+        this.price = price;
+    }
+}
+class Wallet {
+    constructor(charges = []) {
+        this.charges = charges;
+    }
+    add(charge) {
+        this.charges.push(charge);
+        return this;
+    }
+    get gold() {
+        return this.charges.reduce((sum, charge) => sum += charge.price, 0);
+    }
+}
+class Player extends Fighter {
+    constructor(attacks = []) {
+        super(attacks);
+        this.wallet = new Wallet();
+    }
+}
+class Health {
+    constructor() {
+        this.INITIAL_HEALTH = 100;
+    }
+    get status() {
+        return this.remaining === 0 ? 'dead' : 'alive';
+    }
+    get missing() {
+        return this.INITIAL_HEALTH - this.remaining;
+    }
+}
+class FighterHealth extends Health {
+    constructor(attacks = []) {
+        super();
+        this.attacks = attacks;
+    }
+    register(attack) {
+        this.attacks.push(attack);
+        return this;
+    }
+    get remaining() {
+        const damage = this.attacks.reduce((sum, attack) => sum += attack.damage, 0);
+        return this.INITIAL_HEALTH - damage;
+    }
+}
+const p1 = new Player();
+const p2 = new Player();
+const attack = p1.attack(p2);
+console.log(attack.damage === p2.health.missing);
--- index.ts
+++ index.ts
@@ -6,26 +6,28 @@ interface IAttack {
 
 type HealthStatus = 'dead' | 'alive';
 
-interface IHealthBar {
+interface IHealth {
     status: HealthStatus;
-    health: number;
+    remaining: number;
+    missing: number;
 }
 
-interface IPlayerHealthBar extends IHealthBar {
-    register: (attack: IAttack) => IHealthBar;
+interface IFighterHealth extends IHealth {
+    register: (attack: IAttack) => IHealth;
 }
 
 interface IAttacker {
-    attack: (defender: IDefender) => IAttack;
+    attack: (defender: IDefender) => IDefender;
     power: number;
 }
 
 interface IDefender {
-    defend: (attack: IAttack) => IHealthBar;
+    defend: (attack: IAttack) => IHealth;
+    health: IHealth;
     resistance: number;
 }
 
-interface IPlayer extends IAttacker, IDefender {
+interface IFighter extends IAttacker, IDefender {
 
 }
 
@@ -33,55 +35,137 @@ interface IWall extends IDefender {
 
 }
 
-class Attack implements IAttack {
-    constructor(
-        public readonly attacker: IAttacker,
-        public readonly defender: IDefender
-    ) {}
+interface ICharge {
+    price: number;
+}
 
+interface IWallet {
+    add(charge: Charge): IWallet;
+    gold: number;
+}
+
+interface IBuyer {
+    wallet: IWallet;
+}
+
+interface IItemStore {
+    transact: (request: IItemPurchaseRequest) => IItem | false;
+}
+
+interface IItemPurchaseRequest {
+    item: IItem;
+    buyer: IBuyer;
+}
+
+interface IItem {
+    value: number;
+    type: string;
+}
+
+interface IPlayer extends IFighter, IBuyer {
+
+}
+
+class Attack implements IAttack {
+    constructor(public readonly attacker: IAttacker, public defender: IDefender) {}
     get damage() {
         return this.attacker.power - this.defender.resistance;
     }
 }
 
-class Player implements IPlayer {
-    private readonly healthBar: IPlayerHealthBar;
+class Fighter implements IFighter {
+    private readonly _health: IFighterHealth;
     constructor(attacks: IAttack[]) {
-        this.healthBar = new PlayerHealthBar(attacks);
+        this._health = new FighterHealth(attacks);
     }
     protected get basePower() { return 2.0 };
     protected get baseResistance() { return 1.0 };
-    attack(defender: IDefender): IAttack {
-        return new Attack(this, defender);
+    attack(defender: IDefender): IDefender {
+        const attack = new Attack(this, defender);
+        defender.defend(attack);
+        return defender;
     }
     get power() {
         return this.basePower;
     }
-    defend(attack: IAttack): IHealthBar {
-        return this.healthBar.register(attack);
+    defend(attack: IAttack): IHealth {
+        return this.health.register(attack);
     }
     get resistance() {
         return this.baseResistance;
     }
-}
-
-abstract class HealthBar implements IHealthBar {
-    get status(): HealthStatus {
-        return this.health === 0 ? 'dead' : 'alive'
+    get health() {
+        return this._health;
     }
-    abstract get health(): number;
 }
 
-class PlayerHealthBar extends HealthBar implements IPlayerHealthBar {
-    constructor(private attacks: IAttack[]) {
+interface IInventory {
+
+}
+
+class Inventory {
+    items: IItem[];
+    wallet: IWallet;
+}
+
+class Charge implements ICharge {
+    constructor(public readonly price: number) {}
+}
+
+class Wallet implements IWallet {
+    constructor(private readonly charges: ICharge[] = []) {}
+    add(charge: ICharge) {
+        this.charges.push(charge);
+        return this;
+    }
+    get gold(): number {
+        return this.charges.reduce((sum, charge) => sum += charge.price, 0);
+    }
+}
+
+class Player extends Fighter implements IPlayer {
+    public readonly wallet: IWallet;
+    constructor(attacks: IAttack[] = []) {
+        super(attacks);
+        this.wallet = new Wallet();
+    }
+}
+
+abstract class Health implements IHealth {
+    protected readonly INITIAL_HEALTH = 100;
+    get status(): HealthStatus {
+        return this.remaining === 0 ? 'dead' : 'alive'
+    }
+    abstract get remaining(): number;
+    get missing(): number {
+        return this.INITIAL_HEALTH - this.remaining;
+    }
+}
+
+class FighterHealth extends Health implements IFighterHealth {
+    constructor(private attacks: IAttack[] = []) {
         super();
     }
-    register(attack: IAttack) {
+    register(attack: IAttack): IHealth {
         this.attacks.push(attack);
         return this;
     }
-    get health(): number {
+    get remaining(): number {
         const damage = this.attacks.reduce((sum, attack) => sum += attack.damage, 0);
-        return 100 - damage;
+        return this.INITIAL_HEALTH - damage;
     }
 }
+
+interface ITurn {
+    
+}
+
+interface IBattle {
+    next: (turn: ITurn) => ITurn[];
+}
+
+const p1: IPlayer = new Player();
+const p2: IPlayer = new Player();
+
+const defender = p1.attack(p2);
+console.log(defender.health.missing);
