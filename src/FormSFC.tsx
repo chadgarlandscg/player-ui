e161112e7a72812367536f401054d0012faabb73
chadgarlandscg <chadgarlandscg@gmail.com>
2020-12-12 11:36:59 -0700

    Update FormSFC.tsx

--- src/FormSFC.tsx
+++ src/FormSFC.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type FormSFCProps = {
     inputValue: string;
@@ -10,8 +10,27 @@ type FormSFCProps = {
 export function FormSFC(props: FormSFCProps) {
     return (
         <div>
-            <input value={props.inputValue} onChange={props.onInputChange}/>
-            <button onClick={props.onButtonClick}>{props.buttonTitle || "Test!"}</button>
+            <FormInput onInputChange={props.onInputChange} inputValue={props.inputValue} altValidationColor="green"/>
+            <button disabled={!props.inputValue} onClick={props.onButtonClick}>{props.buttonTitle || "Test!"}</button>
         </div>
     );
+}
+
+type FormInputProps = {
+    inputValue: string;
+    onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    altValidationColor?: string;
+}
+
+function FormInput(props: FormInputProps) {
+    const [touched, setTouched] = useState(false);
+    const isInvalid = touched && !props.inputValue;
+    return <input
+        style={isInvalid ? {borderColor: props.altValidationColor || 'red'} : null}
+        value={props.inputValue}
+        onChange={(event) => {
+            setTouched(true);
+            props.onInputChange(event);
+        }
+    }/>
 }
