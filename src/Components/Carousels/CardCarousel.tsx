8c8e121f47fc5b9e968d5422d7c05e3896abacb2
chadgarlandscg <chadgarlandscg@gmail.com>
2020-12-12 15:52:26 -0700

    Add CardCarousel

---
+++ src/Components/Carousels/CardCarousel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HorizontalGrid } from '../Grids/HorizontalGrid'
+
+export const CardCarousel: React.FC = () => {
+  return (
+    <HorizontalGrid>
+        {[
+            
+        ]}
+    </HorizontalGrid>
+  );
+}
