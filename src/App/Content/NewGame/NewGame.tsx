4dc489e8c858cb3a467e3f6a5a06a7e531d19ca1
chadgarlandscg <chadgarlandscg@gmail.com>
2020-12-23 16:54:40 -0700

    Center new game form

--- src/App/Content/NewGame/NewGame.tsx
+++ src/App/Content/NewGame/NewGame.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import { Form } from '../../../Components/Forms/Form';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -20,14 +20,23 @@ interface NewGameProps {
 
 export const NewGame: React.FC<NewGameProps> = () => {
     const classes = useStyles();
+    const formProps = {
+        inputFields: ["Lobby Name"],
+        sliders: [
+            {title: "Max Players", min: 2, max: 6, init: 4},
+            {title: "Max Rounds", min: 1, max: 5, init: 3},
+        ],
+        submitText: "Create Game",
+        onSubmit: console.log
+    };
+    const newGameForm = <Form {...formProps}/>
     return (
-        <Form
-            inputFields={["Lobby Name"]}
-            sliders={[
-                {title: "Max Players", min: 2, max: 6, init: 4},
-                {title: "Max Rounds", min: 1, max: 5, init: 3},
-            ]}
-            submitText="Create Game" onSubmit={console.log}
-        />
+        <Grid container justify={'center'}>
+            <Grid item lg={4}>
+                {newGameForm}
+            </Grid>
+        </Grid>
+            
+    
     );
 }
