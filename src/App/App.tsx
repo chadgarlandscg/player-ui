9c32c7adf5e7429f50ac03db015f098cff5e3067
chadgarlandscg <chadgarlandscg@gmail.com>
2020-12-25 13:31:02 -0700

    Swap colors

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
-import { amber, cyan } from '@material-ui/core/colors';
+import { amber, cyan, green } from '@material-ui/core/colors';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Content } from './Content/Content';
@@ -9,8 +9,8 @@ import { Header } from './Header/Header';
 export const App: React.FC = () => {
     const theme = createMuiTheme({
         palette: {
-            primary: cyan,
-            secondary: amber
+            primary: green,
+            secondary: amber,
         }
     });
     return (
